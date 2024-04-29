a8a9a754007c4a5b666b02829bd805d65f3422da
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-04-29 09:56:20 -0700

    Q 03 ts/js node js of 45 assighnment
    Q 03 ts/js node js of 45 assighnment

---
+++ Q3.js
@@ -0,0 +1,6 @@
+//3.Name Cases: Store a person’s name in a variable, nd
+// then print that person’s name in lowercase, uppercase, and titlecase.
+var firstname = "sonia vaswani";
+console.log(firstname.toLowerCase());
+console.log(firstname.toUpperCase());
+console.log(firstname.charAt(0).toUpperCase() + firstname.slice(1));
---
+++ Q3.ts
@@ -0,0 +1,8 @@
+//3.Name Cases: Store a person’s name in a variable, nd
+// then print that person’s name in lowercase, uppercase, and titlecase.
+
+let firstname: string = "sonia vaswani";
+console.log(firstname .toLowerCase());
+console.log(firstname.toUpperCase());
+console.log(firstname.charAt(0).toUpperCase()+firstname.slice(1));
+
